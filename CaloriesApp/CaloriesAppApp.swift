07cf8cb67585697a83ec2039c24682455ca08531
CoderEslam <eslamghazy600@gmail.com>
2023-09-03 20:59:44 +0200

    Initial Commit

---
+++ CaloriesApp/CaloriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesAppApp.swift
+//  CaloriesApp
+//
+//  Created by Eslam Ghazy on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
